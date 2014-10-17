0fff7032b05ff2bfef397d5a688a12d0fc14ad20
ylf <ylf@ylf-VirtualBox.(none)>
2014-10-17 15:19:21 +0800

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015014647) do
+ActiveRecord::Schema.define(version: 20141017070445) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
-    t.string   "content",     limit: 20000
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
